7bf73fdd43f7ec7e0b1948c572773008f333dc44
Sidney Ferracin Jr. <sidneyferracinjr@gmail.com>
2024-10-11 19:04:29 -0300

    feat: :sparkles: models and adjusts

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# o usuario faz chamada por link e chega aqui
-def home(request):
-    return render(request, 'todos/home.html') # jeito correto de chamar a pagina html de templates
+
+def todo_list(request):
+    return render(request, "todos/todo_list.html")
