16c14b983e2a7f48d91b189314be55abad2f6109
Sidney Ferracin Jr. <sidneyferracinjr@gmail.com>
2024-10-11 20:57:58 -0300

    feat: :sparkles: listing page

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 def todo_list(request):
-    return render(request, "todos/todo_list.html")
+    todos = Todo.objects.all()  # pegar todos os dados do banco de dados
+    return render(request, "todos/todo_list.html", {"todos": todos})
