785d3ade8ac4868808ab945785fd773bcb698e92
Sidney Ferracin Jr. <sidneyferracinjr@gmail.com>
2024-10-12 09:41:12 -0300

    feat: :sparkles: final of listing page

--- todos/models.py
+++ todos/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # essa classe 'models.py' possui ligação direta com as entidades do python
 # toda classe model para ser reconhecida precisa herdar a model principal
 
-class Todo(models.Model): # herdando model principal
+
+class Todo(models.Model):  # herdando model principal
     title = models.CharField(max_length=100, null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=False, blank=False)
-    finished_at = models.DateTimeField(null=True)
+    created_at = models.DateField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(null=False, blank=False)
+    finished_at = models.DateField(null=True)
